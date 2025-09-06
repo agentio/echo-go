d6d3cfbaa6667ec5b83d78913829b9d48ee6d2a6
Tim Burks <timburks@me.com>
2025-09-06 16:13:41 -0700

    test all server/client combinations

--- echo_test.go
+++ echo_test.go
@@ -10,10 +10,72 @@ import (
 	"github.com/agentio/echo-go/internal/commands"
 )
 
-func TestServices(t *testing.T) {
+func TestSocketGrpcServiceGrpcClient(t *testing.T) {
+	test_services(t,
+		[]string{"serve", "grpc", "--socket", "@echotest"},
+		"unix:@echotest",
+		"grpc",
+	)
+}
+
+func TestLocalGrpcServiceGrpcClient(t *testing.T) {
+	port := "19876"
+	test_services(t,
+		[]string{"serve", "grpc", "--port", port},
+		"localhost:"+port,
+		"grpc",
+	)
+}
+
+func TestLocalGrpcServiceConnectGrpcClient(t *testing.T) {
+	port := "19875"
+	test_services(t,
+		[]string{"serve", "grpc", "--port", port},
+		"localhost:"+port,
+		"connect-grpc",
+	)
+}
+
+func TestLocalConnectServiceGrpcClient(t *testing.T) {
+	port := "19874"
+	test_services(t,
+		[]string{"serve", "connect", "--port", port},
+		"localhost:"+port,
+		"grpc",
+	)
+}
+
+func TestLocalConnectServiceConnectClient(t *testing.T) {
+	port := "19873"
+	test_services(t,
+		[]string{"serve", "connect", "--port", port},
+		"localhost:"+port,
+		"connect",
+	)
+}
+
+func TestLocalConnectServiceConnectGrpcClient(t *testing.T) {
+	port := "19872"
+	test_services(t,
+		[]string{"serve", "connect", "--port", port},
+		"localhost:"+port,
+		"connect-grpc",
+	)
+}
+
+func TestLocalConnectServiceConnectGrpcWebClient(t *testing.T) {
+	port := "19871"
+	test_services(t,
+		[]string{"serve", "connect", "--port", port},
+		"localhost:"+port,
+		"connect-grpc-web",
+	)
+}
+
+func test_services(t *testing.T, serverArgs []string, address, stack string) {
 	go func() {
 		serveCmd := commands.Cmd()
-		serveCmd.SetArgs([]string{"serve", "grpc", "--socket", "@echotest"})
+		serveCmd.SetArgs(serverArgs)
 		err := serveCmd.Execute()
 		if err != nil {
 			log.Printf("failed to read output from buffer: %v", err)
@@ -25,31 +87,20 @@ func TestServices(t *testing.T) {
 		Expected string
 	}{
 		{
-			Args: []string{"call", "get", "--address", "unix:@echotest"},
-			Expected: `{"text":"Go echo get: hello"}
-`,
+			Args:     []string{"call", "get", "--address", address, "--stack", stack},
+			Expected: expected_get,
 		},
 		{
-			Args: []string{"call", "collect", "--address", "unix:@echotest"},
-			Expected: `{"text":"Go echo collect: hello 0 hello 1 hello 2"}
-`,
+			Args:     []string{"call", "collect", "--address", address, "--stack", stack},
+			Expected: expected_collect,
 		},
 		{
-			Args: []string{"call", "expand", "--address", "unix:@echotest"},
-			Expected: `{"text":"Go echo expand (0): 1"}
-{"text":"Go echo expand (1): 2"}
-{"text":"Go echo expand (2): 3"}
-`,
+			Args:     []string{"call", "expand", "--address", address, "--stack", stack},
+			Expected: expected_expand,
 		},
 		{
-			Args: []string{"call", "stream", "--address", "unix:@echotest"},
-			Expected: `{"text":"Go echo stream (1): hello 0"}
-{"text":"Go echo stream (2): hello 1"}
-{"text":"Go echo stream (3): hello 2"}
-{"text":"Go echo stream (4): hello 3"}
-{"text":"Go echo stream (5): hello 4"}
-{"text":"Go echo stream (6): hello 5"}
-`,
+			Args:     []string{"call", "stream", "--address", address, "--stack", stack},
+			Expected: expected_stream,
 		},
 	}
 	for _, test := range tests {
@@ -70,3 +121,20 @@ func TestServices(t *testing.T) {
 		}
 	}
 }
+
+const expected_get = `{"text":"Go echo get: hello"}
+`
+
+const expected_collect = `{"text":"Go echo collect: hello 0 hello 1 hello 2"}
+`
+const expected_expand = `{"text":"Go echo expand (0): 1"}
+{"text":"Go echo expand (1): 2"}
+{"text":"Go echo expand (2): 3"}
+`
+const expected_stream = `{"text":"Go echo stream (1): hello 0"}
+{"text":"Go echo stream (2): hello 1"}
+{"text":"Go echo stream (3): hello 2"}
+{"text":"Go echo stream (4): hello 3"}
+{"text":"Go echo stream (5): hello 4"}
+{"text":"Go echo stream (6): hello 5"}
+`
